850d87405920c53638b7ac31c79d58d11123e6ec
faishalshidqi <rshidqi6@gmail.com>
2024-01-29 13:20:26 +0700

    add and update stock to report updated

--- lib/common/validators.dart
+++ lib/common/validators.dart
@@ -19,26 +19,3 @@ String? validateLength(String? value, int? length) {
   }
   return null;
 }
-
-String? validateEmail(value) {
-  validateNotEmpty(value);
-  RegExp regExp = RegExp(
-    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$',
-  );
-  if (!regExp.hasMatch(value)) {
-    return 'Masukkan Alamat Email Yang Valid';
-  } else {
-    return null;
-  }
-}
-
-String? validatePhoneNumber(value) {
-  validateNotEmpty(value);
-  RegExp regExp =
-      RegExp(r'(^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$)');
-  if (!regExp.hasMatch(value)) {
-    return 'Masukkan Nomor Telepon Yang Valid';
-  } else {
-    return null;
-  }
-}
